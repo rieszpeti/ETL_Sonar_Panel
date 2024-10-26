03910dec677fea7d12f2021b45a456df059d6a6d
SillySharp <rieszpeti@gmail.com>
2024-10-26 19:58:13 +0200

    add to main

--- Load/src/main.py
+++ Load/src/main.py
@@ -1,6 +1,7 @@
 import load_news
 import load_satellite
 import load_stock_data
+import load_coordinates
 from logging_config import setup_logging
 
 if __name__ == '__main__':
@@ -9,3 +10,4 @@ if __name__ == '__main__':
     load_news.main()
     load_satellite.main()
     load_stock_data.main()
+    load_coordinates.main()
